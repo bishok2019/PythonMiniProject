6451e126dd434e72f20be5d44388c5efbf0b2b11
Bishok Paudel <bishokpaudel57@gmail.com>
2024-07-14 18:27:31 +0545

    Guess your number added

---
+++ guessnumber.py
@@ -0,0 +1,35 @@
+import random
+
+top = input("Enter a max range of number you want to guess: ")
+if top.isdigit:
+    top = int(top)
+    if top<=0:
+        print("Enter a number larger than 0.")
+        quit()
+else:
+    print("Enter a number.")
+    quit()        
+r = random.randint(0, top)
+# print("Random number generated is: ",r) remove comment for easy navigation
+guesses = 0
+while True:
+    guesses+=1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+    
+    else :
+        print("Enter a number.")
+        continue
+
+    if user_guess == r:
+        print("You got it in ",guesses,"guesses!")
+        break     
+    elif user_guess > r:
+        print("You didnot get it! You were above the number")
+    else:
+        print("You didnot get it! You were below the number")
+        
+
+# print("in",guesses,"guesses.")# or the below one
+#print(f"you got it in {guesses} guesses.")# or the below one
+#print("you got it in "+str(guesses)+" guesses.")
